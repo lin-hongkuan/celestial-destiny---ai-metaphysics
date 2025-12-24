19c2b20ffe4dbca6428ea893aebb30f40e9625f2
linhongkuan <linhk8@mail2.sysu.edu.cn>
2025-12-25 04:28:45 +0800

    push

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/celestial-destiny---ai-metaphysics/',
       server: {
         port: 3000,
         host: '0.0.0.0',
